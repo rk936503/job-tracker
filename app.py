7294c2b79418ad178cd5d588be405a23f3b02f1e
rahul <rk936503@gmail.com>
2026-08-02 02:42:09 +0530

    Add config.py and load secrets via environment variables

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from dotenv import load_dotenv
+from config import Config
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route("/")
 def home():
     return "Job Tracker is alive!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=app.config["DEBUG"])
---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    DEBUG = os.environ.get("FLASK_DEBUG", "False") == "True"
