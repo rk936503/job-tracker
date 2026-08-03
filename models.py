359e83a31a35a4c83b32c09c258e14fe5409b48e
rahul <rk936503@gmail.com>
2026-08-04 00:47:51 +0530

    Add User model, LoginManager, and its migration

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from extensions import db
 from datetime import date
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class JobApplication(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,3 +14,15 @@ class JobApplication(db.Model):
 
     def __repr__(self):
         return f"<JobApplication {self.company} - {self.role}>"
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
